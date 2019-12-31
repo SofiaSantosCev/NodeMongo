f9e40b6ebc786fe3f508fd36e6f4c27259b3c934
Sofía Santos Melián <sofiasantosmelian@gmail.com>
2019-12-31 10:24:27 +0100

    first commit

---
+++ src/models/task.js
@@ -0,0 +1,14 @@
+import mongoose from 'node_modules/mongoose';
+
+const Schema = mongoose.Schema;
+
+const taskSchema = new Schema({
+    title: String,
+    description: String,
+    status: {
+        type: Boolean,
+        default: false
+    }
+});
+
+module.exports = mongoose.model('task', taskSchema);
